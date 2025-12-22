bc4f576557bbce1c45ff2f6d5c49fa75b11c65aa
Devika Polavarapu <153899280+devikapolavarapu@users.noreply.github.com>
2025-12-22 22:55:32 +0530

    Time: 6 ms (100%), Space: 44.4 MB (37.18%) - LeetHub

--- 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.java
+++ 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.java
@@ -1,27 +1,24 @@
 class Solution {
     public int minDeletionSize(String[] strs) {
-        int n = strs.length, m = strs[0].length();
-        int size = 0;
-        int[] dp = new int[m];
-        for (int i = 0; i < m; i++) {
-            dp[i] = 1;
-        }
-        int max_keep = 1;
-        for (int i = 0; i < m; i++) {
+        int n = strs[0].length(), m = strs.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                boolean valid = true;
-                for (int k = 0; k < n; k++) {
-                    if (strs[k].charAt(j) > strs[k].charAt(i)) {
-                        valid = false;
-                        break;
-                    }
-                }
-                if (valid) {
+                if (isValid(strs, j, i)) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-            max_keep = Math.max(max_keep, dp[i]);
         }
-        return m - max_keep;
+        int max = 0;
+        for (int val : dp) max = Math.max(max, val);
+        return n - max;
     }
+    private boolean isValid(String[] strs, int j, int i) {
+        for (String s : strs) {
+            if (s.charAt(j) > s.charAt(i)) return false;
+        }
+        return true;
+    }
+
 }
