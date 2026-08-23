22fe858eb55130ce5b20ceedfd3b6e91f3284e96
Devika Polavarapu <153899280+devikapolavarapu@users.noreply.github.com>
2026-08-23 21:50:27 +0530

    Time: 7 ms (79.56%), Space: 47 MB (64.23%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int leftSum = 0, rightSum = 0;
+        int leftQ = 0, rightQ = 0;
+        int n = num.length();
+        for (int i = 0; i < n; i++) {
+            char c = num.charAt(i);
+            if (i < n / 2) {
+                if (c == '?') leftQ++;
+                else leftSum += c - '0';
+            } else {
+                if (c == '?') rightQ++;
+                else rightSum += c - '0';
+            }
+        }
+        if ((leftQ + rightQ) % 2 != 0) {
+            return true;
+        }
+        int sumDiff = leftSum - rightSum;
+        int qDiff = rightQ - leftQ;
+        return sumDiff * 2 != qDiff * 9;
+    }
+}
