ab970b62a21de265cad6725d3d5f97d3110d695f
Devika Polavarapu <153899280+devikapolavarapu@users.noreply.github.com>
2026-09-06 12:34:17 +0530

    Time: 2 ms (99.97%), Space: 57.8 MB (13.4%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,50 +1,18 @@
 class Solution {
-    public int numDistinct(String s, String t) {    
-        return bottom(s, t);
+    public int numDistinct(String B, String A) {
+         int lenA= A.length();
+        int lenB = B.length();
+        Integer [][] memo = new Integer [lenA][lenB];
+        return solve(A,B,lenA-1,lenB-1,memo);
     }
-    int rec(String s, String t, int n, int m) {
-        if (m < 0)
-            return 1;
-        if (n < 0)
-            return 0;
-        int inc = 0;
-        if (s.charAt(n) == t.charAt(m)) {
-            inc = rec(s, t, n - 1, m - 1);
+    private int solve(String A, String B, int i, int j, Integer[][]memo){
+        if(i<0) return 1;
+        if(j<0) return 0;
+        if(j<i) return 0;
+        if(memo [i][j] != null) return memo [i][j];
+        if(A.charAt(i) == B.charAt(j)){
+            return memo [i][j] = solve(A,B,i-1,j-1,memo) + solve(A,B,i,j-1,memo);
         }
-        int exc = rec(s, t, n - 1, m);
-        return inc + exc;
-    }
-    int top(String s, String t, int n, int m, int[][] dp) {
-        if (m < 0)
-            return 1;
-        if (n < 0)
-            return 0;
-        if (dp[n][m] != -1)
-            return dp[n][m];
-        int inc = 0;
-        if (s.charAt(n) == t.charAt(m)) {
-            inc = top(s, t, n - 1, m - 1, dp);
-        }
-        int exc = top(s, t, n - 1, m, dp);
-        return dp[n][m] = inc + exc;
-    }
-    int bottom(String s, String t) {
-        int r = s.length();
-        int c = t.length();
-        int[][] dp = new int[r + 1][c + 1];
-        for (int i = 0; i <= r; i++) {
-            dp[i][0] = 1;
-        }
-        for (int i = 1; i <= r; i++) {
-            for (int j = 1; j <= c; j++) {
-                int inc = 0;
-                if (s.charAt(i-1) == t.charAt(j-1)) {
-                    inc = dp[i-1][j-1];
-                }
-                int exc = dp[i-1][j];
-                dp[i][j] = inc + exc;
-            }
-        }
-        return dp[r][c];
+        return memo[i][j] = solve(A,B,i,j-1,memo);
     }
 }
